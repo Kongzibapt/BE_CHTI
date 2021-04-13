7e4ba64c5aea41e1f244fe83b1a4ef9f58c438de
Kongzibapt <53861741+Kongzibapt@users.noreply.github.com>
2021-04-13 18:12:16 +0200

    GestionSon end

---
+++ PjtKEIL_StepSon/Src/GestionSon.h
@@ -0,0 +1,7 @@
+void CallbackSon(void) 
+/* A partir des valeurs du tableau Son, 
+convertit les valeurs pour qu'elles soient entre 0 et 719 (*45/4096) 
+et on met ces valeurs dans SortieSon */
+
+void StartSon(void)
+// Lance la séquence : remet l'index à 0
